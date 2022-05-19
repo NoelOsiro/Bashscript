fc9dc9f95ffa394090a37aa02c51f39f92593270
NoelOsiro <noel.osiro@azubi.org>
2022-05-19 20:42:42 +0300

    Third Commit

--- gitpush.sh
+++ gitpush.sh
@@ -23,7 +23,7 @@ read -r message
 
     git commit -m "$message"
     git push git@github.com:NoelOsiro/"$repo".git;
-    figlet "HAVE A NICE DAY!!"
+    echo "HAVE A NICE DAY!!"
     sleep 2
     exit
 
